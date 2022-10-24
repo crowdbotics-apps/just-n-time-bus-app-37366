1fb6adaed1918670c85114bae6ca73a9430ce65c
Crowdbotics <team@crowdbotics.com>
2022-10-24 11:57:50 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/just_n_time_bus_app_37366/MainActivity.java
@@ -0,0 +1,15 @@
+package com.just_n_time_bus_app_37366;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "just_n_time_bus_app_37366";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "just_n_time_bus_app_37366",
+  url: "https://just_n_time_bus_app_37366.botics.co",
+  api: "https://just_n_time_bus_app_37366.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
